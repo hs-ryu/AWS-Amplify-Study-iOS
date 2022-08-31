d966c8e30981fb2ccb4992cfb04d4e8dbd4d5228
ryu <munam4@naver.com>
2022-09-01 00:34:57 +0900

    initial commit

---
+++ iOS Getting Started/iOS Getting Started/iOS_Getting_StartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Getting_StartedApp.swift
+//  iOS Getting Started
+//
+//  Created by ryu hyunsun on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Getting_StartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
