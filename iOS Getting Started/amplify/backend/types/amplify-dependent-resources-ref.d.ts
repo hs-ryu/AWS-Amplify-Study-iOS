30d780975e8d32ec4b5f4918bdb0b2e66168e96b
ryu <munam4@naver.com>
2022-09-01 15:24:34 +0900

    220901 AWS Amplify 학습

---
+++ iOS Getting Started/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "iosgettingstartedea702222": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "iosgettingstarted": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "image": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- iOS Getting Started/iOS Getting Started/iOS_Getting_StartedApp.swift
+++ iOS Getting Started/iOS Getting Started/iOS_Getting_StartedApp.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 @main
 struct iOS_Getting_StartedApp: App {
+    // UIKit 에서 AppDelegate.swift -> application(:didFinishLaunchingWithOptions:)
+    // SwiftUI에서는 앱파일에서 init()에 세팅.
+    init() {
+        let _ = Backend.initialize()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
